637e8b8fb95d8f847ce48f1cb5305329e712dd24
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-10-07 11:47:59 +0300

    update App page

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,27 +1,12 @@
 <template>
-	<Button></Button>
-	<Input v-model.trim="msg" name="test" label="Имя" @update="onInput" />
-	<p>{{ msg }}</p>
-	<Select v-model="selected" :options="['Все', 'Частные']" @update="onSelect" />
-	<p>{{ selected }}</p>
+	<TheHeader />
+	<main>
+		<RouterView />
+	</main>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Button from "./components/Button.vue";
-import Input from "./components/Input.vue";
-import Select from "./components/Select.vue";
-
-const msg = ref("");
-const selected = ref("");
-const onInput = (updatedValue: string) => {
-	msg.value = updatedValue;
-};
-const onSelect = (updatedValue: string) => {
-	console.log(updatedValue);
-	selected.value = updatedValue;
-};
-defineEmits<{ (foo: "foo"): string }>();
+import TheHeader from "./layout/TheHeader.vue";
 </script>
 
 <style scoped></style>
---
+++ src/app/layout/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+	<header class="header">
+		<p>Logo</p>
+		<nav>
+			<ul class="header-list">
+				<li>
+					<RouterLink to="/">Заявки</RouterLink>
+				</li>
+				<li>
+					<RouterLink to="/help">Помощь</RouterLink>
+				</li>
+				<li>
+					<RouterLink to="/messages">Сообщения</RouterLink>
+				</li>
+				<li>
+					<RouterLink to="/login" @click="logout">Выход</RouterLink>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script setup lang="ts">
+import { LOGIN_STORE } from "@/login/store/constants";
+import { useStore } from "vuex";
+
+const store = useStore();
+const logout = () => store.commit(LOGIN_STORE.MUTATIONS.RESET_LOGIN);
+</script>
+
+<style scoped>
+.header {
+	display: flex;
+	justify-content: space-between;
+}
+.header-list {
+	display: flex;
+}
+</style>
